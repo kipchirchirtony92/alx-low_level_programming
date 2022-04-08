c6002e339b457558538160a1cd49db4dc9bf777b
kipchirchirtony92 <kipchirchirtony92@gmail.com>
2022-04-08 13:21:03 -0700

    Hotfix 0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,10 @@
 #include <time.h>
 
 /**
- * main: determine if number is positive or negative
+ * main - determine if number is positive or negative
  *
- * Description: program will assign a random number to n when executed.
+ * Description: program will assign a random number to the variable n each time
+ *it is executed
  *
  * Return: Always 0 Success
  */
