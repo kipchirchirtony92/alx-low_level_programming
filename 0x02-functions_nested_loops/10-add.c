14b5d54864cfb4aa1ca6d5daf8385918d67d7b2b
kipchirchirtony92 <kipchirchirtony92@gmail.com>
2022-04-12 12:37:26 -0700

    Add a prototype unction that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @num1: int parameter 1
+ * @num2: int parameter 2
+ *
+ * Description: given as two integers as arguments it returns sum
+ *
+ * Return: sum fo 2 numbers
+ */
+
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+
+	return (sum);
+}
