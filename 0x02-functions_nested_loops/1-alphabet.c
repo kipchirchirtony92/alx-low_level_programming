e238411d8534ef68cc7e5373665747787bd28275
kipchirchirtony92 <kipchirchirtony92@gmail.com>
2022-04-12 10:02:21 -0700

    Add source code that prints alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+
+	_putchar('\n');
+}
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
