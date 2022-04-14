907c04a1a9be9bcb275912238d146f37ff9a70c9
kipchirchirtony92 <kipchirchirtony92@gmail.com>
2022-04-14 00:12:28 -0700

    Task 0 :Added a test function for positive_negative function(int n)

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *main -test function that prints is a number is positive or negative
+ *
+ *Return: Always 0 sucess
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	positive_or_negative(n);
+
+	return (0);
+}
