847994c1b2b88b4558a995194f828e050ddcd82f
kipchirchirtony92 <kipchirchirtony92@gmail.com>
2022-04-14 11:19:56 -0700

    Added a function to solve the fizz buzz problem

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - solution to fizz buzz problem
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 1;
+
+	printf("%d ", i);
+
+	for (i = 2; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i == 5)
+			printf("Fizz Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
