bb76ffb713c0225d10dc05f8c8f0c4e8908cd528
kipchirchirtony92 <kipchirchirtony92@gmail.com>
2022-04-20 05:34:14 -0700

    Hotfix: 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,28 +6,19 @@
  *
  * @s: str param to be printed in reverse
  */
-int _strlen(char *s);
 
 void print_rev(char *s)
 {
-	int len = _strlen(s);
-	while (len >= 0)
+	int i = 0;
+
+	while (s[i] != '\0')
 	{
-		_putchar(s[len]);
-		len--;
+		i++;
+	}
+
+	while (i--)
+	{
+		_putchar(s[i]);
 	}
 	_putchar('\n');
-}
-
-int _strlen(char *s)
-{
-        int len;
-        int i;
-
-        len = 0;
-        for (i = 0; s[i] != '\0'; i++)
-        {
-                len++;
-        }
-        return (len);
 }
