491c75d3b27c707f621d127ce7ee555d44fd9cd9
Tony <kipchirchirtony92@gmail.com>
2022-05-24 06:29:20 -0700

    Added a function that prints a string before the main function executes

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
-void __attribute__((constructor)) hare(void);
-
 /**
  * hare - prints a string before the main function executes
  */
 
-void hare(void)
+void __attribute__((constructor)) hare(void)
 {
 	printf("You're beat! and yet, you must allow,\\nI bore my house upon");
 	printf("my back!\\n\n");
