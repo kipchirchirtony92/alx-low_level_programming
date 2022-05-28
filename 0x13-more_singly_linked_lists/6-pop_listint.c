229d153145ca0170a5e9a6b3edfbb9c9703bd5c3
Tony <kipchirchirtony92@gmail.com>
2022-05-28 04:12:33 -0700

    Hotfix

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,16 +12,16 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *tmp;
+	int ret;
 
 	if (*head == NULL)
 		return (0);
 
 	tmp = *head;
-
-
+	ret = (*head)->n;
 	*head = (*head)->next;
 
 	free(tmp);
 
-	return ((*head)->n);
+	return (ret);
 }
