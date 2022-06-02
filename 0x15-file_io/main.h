f787a82094da0010e4904f39249b9b1ee216c187
Tony <kipchirchirtony92@gmail.com>
2022-06-02 04:13:48 -0700

    Added the main.h header file containing all the function prototypes

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Desciption - Header file containing protypes for all the functions
+ *             for the folder 0x14-file_io directory
+*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+int create_file(const char *filename, char *text_content);
+
+int append_text_to_file(const char *filename, char *text_content);
+
+
+#endif /* MAIN_H */
