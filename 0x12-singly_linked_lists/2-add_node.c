f8b1595462860a74a2e2325d23064d4aa8d23636
Tony Kipchirchir <kipchirchirtony92@gmail.com>
2023-12-13 15:31:38 +0000

    adds a new node at the beggining of the list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -31,6 +31,11 @@ list_t *add_node(list_t **head, const char *str)
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 	new->len = _strlen(str);
 	new->next = *head;
 	*head = new;
