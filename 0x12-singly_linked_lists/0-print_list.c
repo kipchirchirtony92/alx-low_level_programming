cdf2c0e1a3b9bfff89b364ada579450a1390c91e
Tony Kipchirchir <kipchirchirtony92@gmail.com>
2023-12-13 13:42:32 +0000

    A function that counts no of elements in alist and return the number of nodes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,9 +10,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes;
-
-	nodes = 0;
+	size_t count = 0;
 
 	while (h)
 	{
@@ -20,8 +18,9 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		nodes++;
+		count++;
 		h = h->next;
 	}
-	return (nodes);
+	return (count);
 }
+
